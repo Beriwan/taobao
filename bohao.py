5860a5f2279e672ab485e3c23a924db55b57ac7f
Beriwan <1024394026@qq.com>
2018-04-27 16:34:16 +0800

    post9

--- bohao.py
+++ bohao.py
@@ -1,8 +1,51 @@
 import subprocess
 import re
+
+import requests
+from requests import ReadTimeout
+
+from taobao.ipdb import *
 import redis
 
 ADSL_IFNAME = 'adsl-stop;adsl-start'
+
+
+def remove_proxy():
+    """
+    移除代理
+    :return: None
+    """
+    redis = RedisClient()
+    redis.remove('adsl1')
+    print('Successfully Removed Proxy')
+
+
+def set_proxy(proxy):
+    """
+    设置代理
+    :param proxy: 代理
+    :return: None
+    """
+    redis = RedisClient()
+    if redis.set('adsl1', proxy):
+        print('Successfully Set Proxy', proxy)
+
+
+# def test_proxy(self, proxy):
+#     """
+#     测试代理
+#     :param proxy: 代理
+#     :return: 测试结果
+#     """
+#     try:
+#         response = requests.get('www.baidu.com', proxies={
+#             'http': 'http://' + proxy,
+#             'https': 'https://' + proxy
+#         }, timeout=10)
+#         if response.status_code == 200:
+#             return True
+#     except (ConnectionError, ReadTimeout):
+#         return False
 
 
 def get_ip():
@@ -16,12 +59,15 @@ def get_ip():
 
 
 def main():
+    remove_proxy()
     (status, output) = subprocess.getstatusoutput(ADSL_IFNAME)
     if status == 0:
         print('ADSL Successfully')
         ip = get_ip()
         if ip:
             print('ip is:' + ip)
+            proxy = '{ip}:{port}'.format(ip=ip, port=8888)
+            set_proxy(proxy)
         else:
             print('get ip failed')
     else:
@@ -30,4 +76,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
